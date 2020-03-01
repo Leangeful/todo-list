acbe895c5578457768566dbcb45b5b2cf7b074c1
Leange <cuttheleash1@gmail.com>
2020-03-01 19:05:59 +0100

    Updates

---
+++ precache-manifest.0e3767d0eeaad4023b6d7d9fd141e2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa118cac54a3ac2975bc9fb6f55527f2",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "9d56615f37fdea829005",
+    "url": "/todo-list/static/css/main.b310fb38.chunk.css"
+  },
+  {
+    "revision": "19fa4fbbf623991a8c0c",
+    "url": "/todo-list/static/js/2.90e88e26.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/todo-list/static/js/2.90e88e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d56615f37fdea829005",
+    "url": "/todo-list/static/js/main.2376105a.chunk.js"
+  },
+  {
+    "revision": "75ccf8ee5e207cc72fea",
+    "url": "/todo-list/static/js/runtime-main.2948125f.js"
+  }
+]);
